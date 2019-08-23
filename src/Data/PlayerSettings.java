61e72c3513e122879366719df7633fd6c27f0422
Ferexx <jack.price99@hotmail.com>
2019-08-23 17:39:21 +0100

    Updated playerSettings

--- src/Data/PlayerSettings.java
+++ src/Data/PlayerSettings.java
@@ -1,6 +1,38 @@
 package Data;
 
-public class PlayerSettings {
-    int volume=50;
+import java.io.*;
 
+public class PlayerSettings {
+    File settingsFile;
+    int volume=50;
+    int res=0;
+
+    public PlayerSettings(File settingsFile) {
+        this.settingsFile = settingsFile;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(settingsFile));
+            String st;
+            while((st = br.readLine())!=null) {
+                String split[] = st.split("=");
+                switch(split[0]) {
+                    case "volume":
+                        volume = Integer.parseInt(split[1]);
+                        break;
+                    case "resolution":
+                        res = Integer.parseInt(split[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveSettings() {
+        try {
+            FileWriter fw = new FileWriter(settingsFile, false);
+            fw.write("volume=" + volume);
+            fw.write("resolution=" + res);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
